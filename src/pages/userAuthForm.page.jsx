7fabe4042e7b64baee5b241f25edad6cc55a1747
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-09 14:14:40 +0530

    authentication commit

--- src/pages/userAuthForm.page.jsx
+++ src/pages/userAuthForm.page.jsx
@@ -2,6 +2,9 @@ import InputBox from "../components/input.component";
 import { BiUser } from "react-icons/bi";
 import { MdOutlineAlternateEmail, MdPassword } from "react-icons/md";
 
+import google from "../imgs/google.png";
+import { Link } from "react-router-dom";
+
 const UserAuthForm = ({ type }) => {
   return (
     <div>
@@ -36,6 +39,36 @@ const UserAuthForm = ({ type }) => {
           <button className="btn-dark center mt-14">
             {type.replace("-", " ")}
           </button>
+          <div className="relative w-full items-center flex gap-2 mt-10 opacity-10 uppercase text-black font-bold">
+            <hr className="w-1/2 border-black" />
+            <p>or</p>
+            <hr className="w-1/2 border-black" />
+          </div>
+          <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
+            <img src={google} alt="" className="w-5" />
+            conitune with google
+          </button>
+          {type === "sign-in" ? (
+            <p className="mt-6 text-gray-800 text-xl text-center">
+              Don't have an account
+              <Link
+                to={`/signup`}
+                className="underline text-black text-xl ml-1"
+              >
+                Join us today
+              </Link>
+            </p>
+          ) : (
+            <p className="mt-6 text-gray-800 text-xl text-center">
+              Already a member
+              <Link
+                to={`/signin`}
+                className="underline text-black text-xl ml-1"
+              >
+                Sign in here.
+              </Link>
+            </p>
+          )}
         </form>
       </section>
     </div>

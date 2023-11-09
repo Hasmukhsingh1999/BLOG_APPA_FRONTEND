a1b7702c1f43a6513162b544bcb16a03aa269436
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-09 11:16:58 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/navbar.component"
+
+const App = () => {
+  return (
+    <div className='bg-red-500'>
+     <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar.component.jsx
@@ -0,0 +1,11 @@
+import LOGO from "../imgs/logo.png";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img src={LOGO} alt="" className="w-10 flex-none"/>
+    </nav>
+  );
+};
+
+export default Navbar;

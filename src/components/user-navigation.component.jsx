cc8cfcb7e2a91d8a71ee73a0d548a66d9ac76b5d
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-11 09:35:58 +0530

    animation commit

--- src/components/user-navigation.component.jsx
+++ src/components/user-navigation.component.jsx
@@ -1,9 +1,17 @@
 
 import AnimationWrapper from '../common/page-animation'
-
+import Link from 'react-router-dom'
 const UserNavigationPanel = () => {
   return (
-    <AnimationWrapper transition={{duration:0.2,y:{duration:0.1}}}>
+    <AnimationWrapper transition={{duration:0.2}}>
+
+      <div className='bg-white absolute right-0 border-grey w-60 overflow-hidden duration-200'>
+<Link to={'/editor'} className="flex gap-2 link">
+
+</Link>
+
+      </div>
+
 
     </AnimationWrapper>
   )

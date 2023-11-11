e422c2411ac79820d4e9541d1badc06b64264ccc
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-11 22:02:36 +0530

    dependency added

--- src/components/blog-editor.component.jsx
+++ src/components/blog-editor.component.jsx
@@ -2,22 +2,26 @@ import { Link } from "react-router-dom";
 import LOGO from "../imgs/logo.png";
 import AnimationWrapper from "../common/page-animation";
 import toast, { Toaster } from "react-hot-toast";
-import { useContext, useState } from "react";
+import { useContext, useDebugValue, useEffect, useState } from "react";
 import axios from "axios";
-import blog from "../imgs/banner.jpg";
+import blogImg from "../imgs/banner.jpg";
 import { EditorContext } from "../pages/editor.page";
 
 const BlogEditorComponent = () => {
   const [bannerUrl, setBannerUrl] = useState("");
 
   let {
+    blog,
     blog: { title, banner, content, tags, des },
     setBlog,
   } = useContext(EditorContext);
 
+  useEffect(() => {
+    // let editor = new Edi
+  }, []);
   const handleBannerUpload = async (event) => {
     const file = event.target.files[0];
-  
+
     if (file) {
       try {
         const formData = new FormData();
@@ -35,9 +39,9 @@ const BlogEditorComponent = () => {
         toast.dismiss(loadingToast);
         toast.success("Uploaded");
         console.log(response.data);
-  
+
         const imageUrl = URL.createObjectURL(file);
-        
+
         // Set the banner property of the blog object to the imageUrl
         setBlog({ ...blog, banner: imageUrl });
       } catch (error) {
@@ -45,7 +49,6 @@ const BlogEditorComponent = () => {
       }
     }
   };
-  
 
   const handleTitleKeyDown = (e) => {
     if (e.keyCode === 13) {
@@ -58,7 +61,6 @@ const BlogEditorComponent = () => {
     input.style.height = "auto";
     input.style.height = input.scrollHeight + "px";
 
- 
     setBlog({ ...blog, title: input.value });
   };
 
@@ -69,7 +71,7 @@ const BlogEditorComponent = () => {
           <img src={LOGO} className="w-10 flex-none" />
         </Link>
         <p className="max-md:hidden text-black line-clamp-1 w-full">
-          { title ?? "New Blog"}
+          {title ?? "New Blog"}
         </p>
         <div className="flex gap-4 ml-auto ">
           <button className="btn-dark py-2">Publish</button>
@@ -83,7 +85,7 @@ const BlogEditorComponent = () => {
             <div className="realative aspect-video hover:opacity-80 bg-white border-4 border-grey">
               <form>
                 <label htmlFor="uploadBanner">
-                  <img src={banner || blog} alt="banner" className="z-20" />
+                  <img src={banner || blogImg} alt="banner" className="z-20" />
                   <input
                     id="uploadBanner"
                     type="file"
@@ -100,6 +102,9 @@ const BlogEditorComponent = () => {
               onKeyDown={handleTitleKeyDown}
               onChange={handleTitleChange}
             ></textarea>
+            <hr className="w-full opacity-10 my-5" />
+
+            <div id="textEditor" className="font-gelasio"></div>
           </div>
         </section>
       </AnimationWrapper>

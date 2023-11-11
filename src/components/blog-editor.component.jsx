22f69b3b7545456e97a3456a481121568e385d33
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-11 19:13:41 +0530

    initial commit

--- src/components/blog-editor.component.jsx
+++ src/components/blog-editor.component.jsx
@@ -4,14 +4,38 @@ import AnimationWrapper from "../common/page-animation";
 
 import { useState } from "react";
 
-
+import axios from "axios";
 
 const BlogEditorComponent = () => {
-    const [bannerUrl, setBannerUrl] = useState("");
+  const [bannerUrl, setBannerUrl] = useState("");
 
-  const handleBannerUpload = async(e) => {
-  
+  const handleBannerUpload = async (event) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      try {
+        const formData = new FormData();
+        formData.append("file", file);
+
+        const response = await axios.post(
+          `${import.meta.env.VITE_SERVER_DOMAIN}/api/upload`,
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
+        console.log(response.data); // You can handle the response from the server as needed
+
+        const imageUrl = URL.createObjectURL(file);
+        setBannerUrl(imageUrl);
+      } catch (error) {
+        console.error("Error uploading file:", error);
+      }
+    }
   };
+
   return (
     <>
       <nav className="navbar">
@@ -28,22 +52,25 @@ const BlogEditorComponent = () => {
         <section>
           <div className="mx-auto max-w-[900px] w-full">
             <div className="realative aspect-video hover:opacity-80 bg-white border-4 border-grey">
-              <label htmlFor="uploadBanner">
-                <img
-                  src={ bannerUrl ||
-                    "https://images.unsplash.com/photo-1699594165148-0eb85ec79666?q=80&w=1791&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  }
-                  alt="banner"
-                  className="z-20"
-                />
-                <input
-                  id="uploadBanner"
-                  type="file"
-                  accept=".png, .jpg, .jpeg"
-                  hidden
-                  onChange={handleBannerUpload}
-                />
-              </label>
+              <form>
+                <label htmlFor="uploadBanner">
+                  <img
+                    src={
+                      bannerUrl ||
+                      "https://images.unsplash.com/photo-1682695797873-aa4cb6edd613?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    }
+                    alt="banner"
+                    className="z-20"
+                  />
+                  <input
+                    id="uploadBanner"
+                    type="file"
+                    accept=".png, .jpg, .jpeg"
+                    hidden
+                    onChange={handleBannerUpload}
+                  />
+                </label>
+              </form>
             </div>
           </div>
         </section>

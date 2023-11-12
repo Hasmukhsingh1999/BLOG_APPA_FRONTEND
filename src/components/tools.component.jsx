493723eab986918ca752814c15d0a3c741f649f9
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-12 14:46:15 +0530

    issues resolved

--- src/components/tools.component.jsx
+++ src/components/tools.component.jsx
@@ -1,3 +1,18 @@
 //importing tools
 
-import Embed from '@editorjs/embed';
+import Embed from "@editorjs/embed";
+import List from "@editorjs/list";
+import Image from "@editorjs/image";
+import Header from "@editorjs/header";
+import Quote from "@editorjs/quote";
+import Marker from "@editorjs/marker";
+import InlineCode from '@editorjs/inline-code';
+
+export const tools = {
+    embed:Embed,
+    list :List,
+    quote:Quote,
+    marker:Marker,
+    inlineCode:InlineCode,
+
+}

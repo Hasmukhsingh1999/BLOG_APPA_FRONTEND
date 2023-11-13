86ad45d6101d12eaca19a417c6fa73726f110ab6
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-13 10:18:31 +0530

    final commit

--- src/components/publish-form.component.jsx
+++ src/components/publish-form.component.jsx
@@ -1,11 +1,29 @@
-import AnimationWrapper from "../common/page-animation"
+import { Toaster, toast } from "react-hot-toast";
+import AnimationWrapper from "../common/page-animation";
+
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { useContext } from "react";
+import { EditorContext } from "../pages/editor.page";
 
 const PublishForm = () => {
+  let { setEditorState } = useContext(EditorContext);
+  const handleCloseEvent = () => {
+    setEditorState("editor");
+  };
   return (
     <AnimationWrapper>
-      
-    </AnimationWrapper>
-  )
-}
+      <section>
+        <Toaster />
 
-export default PublishForm
+        <button
+          className="w-12 h-12 absolute right-[5vw] z-10 top-[5%] lg:top-[10%]"
+          onClick={handleCloseEvent}
+        >
+          <AiOutlineCloseCircle />
+        </button>
+      </section>
+    </AnimationWrapper>
+  );
+};
+
+export default PublishForm;
